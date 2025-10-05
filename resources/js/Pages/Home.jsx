8badbaf145b52704755f75d0afddca1a590d9908
Shirozo <neilbryanbagas@icloud.com>
2025-10-05 23:10:36 +0800

    second commit

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,9 @@
+import AuthenticatedLayout from "../Layouts/AuthenticatedLayout";
+
 export default function Home() {
     return (
-        <>
+        <AuthenticatedLayout>
             <h1>Hello world</h1>
-        </>
+        </AuthenticatedLayout>
     )
 }
