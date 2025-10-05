6bc9e18097e5dd7cf7ad23817ea586a84881484c
Shirozo <neilbryanbagas@icloud.com>
2025-10-05 23:26:43 +0800

    refactor: remove Home component and its associated layout

--- resources/js/Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import AuthenticatedLayout from "../Layouts/AuthenticatedLayout";
-
-export default function Home() {
-    return (
-        <AuthenticatedLayout>
-            <h1>Hello world</h1>
-        </AuthenticatedLayout>
-    )
-}
