44449b25ecf625287834ab681dcd2324a11a69ad
Shirozo <neilbryanbagas@icloud.com>
2025-11-05 23:40:57 +0800

    feat: update authorization logic in UpdatePositionRequest to allow user validation

--- app/Http/Requests/UpdatePositionRequest.php
+++ app/Http/Requests/UpdatePositionRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdatePositionRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdatePositionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return (bool)Auth::user();
     }
 
     /**
@@ -22,7 +23,8 @@ class UpdatePositionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required|string",
+            "salary" => "required|numeric"
         ];
     }
 }
