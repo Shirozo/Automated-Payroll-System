b1d01ee87d18619e9861cb3b0ee811001742d563
Shirozo <neilbryanbagas@icloud.com>
2025-11-05 22:57:45 +0800

    feat: add name and salary columns to positions table in migration

--- database/migrations/2025_11_04_155036_create_positions_table.php
+++ database/migrations/2025_11_04_155036_create_positions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->float("salary");
             $table->timestamps();
         });
     }
