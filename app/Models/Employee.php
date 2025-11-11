cd90abe99602be93bf1dc00f02b7918b677c96d0
Shirozo <neilbryanbagas@icloud.com>
2025-11-11 23:33:12 +0800

    feat: add relationships for user and position in Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -21,5 +21,14 @@ class Employee extends Model
         "device",
         "fingerprint_id",
         "position_id",
+        "user_id"
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function position() {
+        return $this->belongsTo(Position::class);
+    }
 }
