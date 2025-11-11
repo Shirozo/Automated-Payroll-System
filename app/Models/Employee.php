e242d58d80dc9c7dc8c427bd1e17af79b63f9cbc
Shirozo <neilbryanbagas@icloud.com>
2025-11-11 22:42:45 +0800

    feat: update fillable attributes in Employee model for consistency

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,4 +9,17 @@ class Employee extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployeeFactory> */
     use HasFactory;
+    protected $fillable = [
+        "employee_number",
+        "deduction_gsis_mpl",
+        "deduction_pagibig_mp3",
+        "deduction_pagibig_calamity",
+        "deduction_city_savings",
+        "deduction_withholding_tax",
+        "deduction_igp_cottage",
+        "deduction_cfi",
+        "device",
+        "fingerprint_id",
+        "position_id",
+    ];
 }
