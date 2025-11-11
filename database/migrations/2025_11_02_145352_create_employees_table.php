71f84e3658d121c49ef63dc23824de469b7239fd
Shirozo <neilbryanbagas@icloud.com>
2025-11-11 22:42:38 +0800

    feat: add employee fields and foreign key constraints to employees table migration

--- database/migrations/2025_11_02_145352_create_employees_table.php
+++ database/migrations/2025_11_02_145352_create_employees_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('employee_number');
+            $table->float('deduction_gsis_mpl', 2);
+            $table->float('deduction_pagibig_mp3', 2);
+            $table->float('deduction_pagibig_calamity', 2);
+            $table->float('deduction_city_savings', 2);
+            $table->float('deduction_withholding_tax', 2);
+            $table->float('deduction_igp_cottage', 2);
+            $table->float('deduction_cfi', 2);
+            $table->string("device")->nullable();
+            $table->integer("fingerprint_id")->nullable();
+            $table->foreignId('position_id')->nullable()->constrained('positions')->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
