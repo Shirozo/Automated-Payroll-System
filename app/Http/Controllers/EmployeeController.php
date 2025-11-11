5b7e431ea148c658207c716c39565ae5e044d7da
Shirozo <neilbryanbagas@icloud.com>
2025-11-11 23:33:00 +0800

    feat: implement employee creation and display logic in EmployeeController

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreEmployeeRequest;
 use App\Http\Requests\UpdateEmployeeRequest;
 use App\Models\Employee;
 use App\Models\Position;
+use App\Models\User;
 
 class EmployeeController extends Controller
 {
@@ -15,6 +16,24 @@ class EmployeeController extends Controller
     public function store(StoreEmployeeRequest $request)
     {
         //
+        try {
+            $request->validated();
+
+            $user = User::create([
+                "name" => $request->name,
+                "username" => $request->employee_number,
+                "password" => $request->employee_number,
+            ]);
+
+            $employee = Employee::create(array_merge($request->validated(), [
+                "user_id" => $user->id
+            ]));
+            
+            return redirect()->route("employee.show")->with('success', 'Employee created successfully');
+        } catch (\Throwable $th) {
+            return redirect()->route("employee.show")->with('error', $th->getMessage());
+    
+        }
     }
 
     /**
@@ -23,9 +42,14 @@ class EmployeeController extends Controller
     public function show(Employee $employee)
     {
         //
+
+        $employee = Employee::with(['user', 'position'])->get();
+        $nextId = (Employee::max('id') ?? 0) + 1;
         $positions = Position::all();
         return inertia("Employee", [
-            "positions" => $positions
+            "positions" => $positions,
+            "nextId" => date("Y") . "-" . str_pad((string)$nextId, 5, "0", STR_PAD_LEFT),
+            "initialEmployees" => $employee
         ]);
     }
 
