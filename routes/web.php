31b35ab722e12662b4071ca1655b7cecb9a6f69d
Shirozo <neilbryanbagas@icloud.com>
2025-11-14 01:33:13 +0800

    fix: update dashboard route to use DashboardController and adjust middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ConfigurationController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\PositionController;
@@ -18,14 +19,19 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group(["prefix" => "", "as" => "index.", "middleware" => ["auth"]], function() {
+    
+    // Route::get("/", [EmployeeController::class, "show"])->name("show");
+
+    Route::get("/dashboard", [DashboardController::class, "dashboard"])->name("dashboard");
+
+    Route::get("/dashboard/employee", [DashboardController::class, "employee"])->name("employee");
 });
 
 Route::group(["prefix" => "employee", "as" => "employee.", "middleware" => ["auth"]], function() {
