31199ee81401db3b57022d1225a476e79fdab58c
Shirozo <neilbryanbagas@icloud.com>
2025-11-26 00:57:59 +0800

    fix: format routing and middleware configuration in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use \App\Http\Middleware\HandleInertiaRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -19,6 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             HandleInertiaRequests::class,
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            'attendance/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
