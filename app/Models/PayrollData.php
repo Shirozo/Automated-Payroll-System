caf5bd91e242a5377dad031dfb4aceb61f4f55e9
Shirozo <neilbryanbagas@icloud.com>
2026-03-04 00:09:22 +0800

    feat: create PayrollData model with fillable attributes for payroll information

---
+++ app/Models/PayrollData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PayrollData extends Model
+{
+    //
+    protected $fillable = [
+        "payroll_id",
+        "employee_id",
+        "rate",
+        "pera",
+        "period_earned",
+        "gsis_mpl",
+        "philhealth",
+        "local_pave",
+        "life_retirement",
+        "pagibig_premium",
+        "pagibig_mp3",
+        "pagibig_calamity",
+        "city_savings",
+        "absence_wo_pay",
+        "withholding_tax",
+        "cottage_rental",
+        "essu_fa",
+        "retiree_asst",
+        "essu_union",
+        "cfi",
+    ];
+}
