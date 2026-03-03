322391fd5143e76c8375d78cb45b7acca28b728c
Shirozo <neilbryanbagas@icloud.com>
2026-03-04 02:43:25 +0800

    feat: enhance PayrollController to retrieve and display unique attendance dates

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePayrollRequest;
+use App\Models\Attendance;
 use App\Models\Payroll;
 use App\Services\PayrollService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PayrollController extends Controller
@@ -29,8 +31,49 @@ class PayrollController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Payroll $payroll)
+    public function show(Request $request)
     {
         //
+
+        $availableDates = Attendance::select('date')
+            ->get()
+            ->map(function ($attendance) {
+                $date = Carbon::parse($attendance->date);
+                return [
+                    'month' => $date->format('F'),
+                    'year' => $date->year,
+                    'month_num' => $date->month,
+                ];
+            })
+            ->unique(function ($item) {
+                return $item['year'] . '-' . $item['month_num'];
+            })
+            ->sortByDesc(function ($item) {
+                return $item['year'] * 100 + $item['month_num'];
+            })
+            ->values()
+            ->map(function ($item) {
+                return [
+                    'month' => $item['month'],
+                    'year' => $item['year'],
+                ];
+            });
+
+        return inertia("Payroll", [
+            "availableDates" => $availableDates
+        ]);
+    }
+
+    public function destroy(Request $request, Payroll $payroll)
+    {
+        $request->validate([
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $payroll->delete();
+
+        return redirect()->route("position.show")->with([
+            "success" => "Payroll Deleted!",
+        ]);
     }
 }
