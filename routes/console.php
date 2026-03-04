e11f25b66015cf379dfaff04938b81dd57c27c68
Shirozo <neilbryanbagas@icloud.com>
2026-03-04 21:12:53 +0800

    feat: add daily task command to run attendance check and schedule it

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Services\AttendanceCheck;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('app:daily-task', function () {
+    (new AttendanceCheck())->checkAttendance();
+})->purpose('Run the daily function');
+
+Schedule::command('app:daily-task')->dailyAt('05:00');
