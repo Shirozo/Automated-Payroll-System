8d1ae89b75292e1c943340b630dc0c5bbf5dc8fe
Shirozo <neilbryanbagas@icloud.com>
2026-03-05 21:25:39 +0800

    feat: update session destruction to redirect to global logout URL

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,14 +39,16 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        $returnTo = urlencode(url('/'));
+        
+        $globalLogoutUrl = "http://192.168.88.247:8000/global-logout?redirect={$returnTo}";
+
+        return Inertia::location($globalLogoutUrl);
     }
 }
