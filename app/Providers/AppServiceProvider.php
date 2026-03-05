5a9a677f98c486c272169d34d1de8d0c39f82a4f
Shirozo <neilbryanbagas@icloud.com>
2026-03-05 17:06:41 +0800

    feat: extend Socialite for Laravel Passport integration in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\LaravelPassport\Provider as LaravelPassportProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Event::listen(function (SocialiteWasCalled $event) {
+            $event->extendSocialite('laravelpassport', LaravelPassportProvider::class); // Updated
+        });
     }
 }
