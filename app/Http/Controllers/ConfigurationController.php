1c9afec95735fcfefc00b3e57008051fe923486e
Shirozo <neilbryanbagas@icloud.com>
2026-03-08 23:49:38 +0800

    feat: add updateAttendance method to handle attendance configuration updates

--- app/Http/Controllers/ConfigurationController.php
+++ app/Http/Controllers/ConfigurationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreConfigurationRequest;
 use App\Http\Requests\UpdateConfigurationRequest;
+use App\Http\Requests\UpdateConfigurationRequestAttendance;
 use App\Models\Configuration;
 
 class ConfigurationController extends Controller
@@ -51,5 +52,38 @@ class ConfigurationController extends Controller
     public function destroy(Configuration $configuration)
     {
         //
+    }
+
+    public function updateAttendance(UpdateConfigurationRequestAttendance $updateConfigurationRequestAttendance)
+    {
+        $updateConfigurationRequestAttendance->validated();
+
+        $morning_login = Configuration::where('name', 'morning_login')->first();
+        $morning_logout = Configuration::where('name', 'morning_logout')->first();
+        $afternoon_login = Configuration::where('name', 'afternoon_login')->first();
+        $afternoon_logout = Configuration::where('name', 'afternoon_logout')->first();
+        $grace_period = Configuration::where('name', 'grace_period')->first();
+
+        $morning_login->update([
+            'value' => $updateConfigurationRequestAttendance->morning_login
+        ]);
+
+        $morning_logout->update([
+            'value' => $updateConfigurationRequestAttendance->morning_logout
+        ]);
+
+        $afternoon_login->update([
+            'value' => $updateConfigurationRequestAttendance->afternoon_login
+        ]);
+
+        $afternoon_logout->update([
+            'value' => $updateConfigurationRequestAttendance->afternoon_logout
+        ]);
+
+        $grace_period->update([
+            'value' => $updateConfigurationRequestAttendance->grace_time
+        ]);
+
+        return redirect()->route("configuration.show")->with('success', 'Configuration updated successfully');
     }
 }
